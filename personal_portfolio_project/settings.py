97b2a1a8e711dbcb0dd27737e3b73d591026263e
maortega2-ops <maortega2@catalyte.io>
2021-08-12 13:42:39 -0600

    damn

--- personal_portfolio_project/settings.py
+++ personal_portfolio_project/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'personal_portfolio_project.wsgi.application'
 if 'RDS_DB_NAME' in os.environ:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.mysql',
+            'ENGINE': 'django.db.backends.postgresql',
             'NAME': os.environ['RDS_DB_NAME'],
             'USER': os.environ['RDS_USERNAME'],
             'PASSWORD': os.environ['RDS_PASSWORD'],
